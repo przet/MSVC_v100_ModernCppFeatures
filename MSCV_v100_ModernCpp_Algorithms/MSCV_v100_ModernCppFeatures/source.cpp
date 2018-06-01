61c68b2a16f2989a806d4ce8ce8e6c308dd675ee
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2018-06-01 11:31:50 +0930

    the new solution name folder (with files from old solution folder)

---
+++ MSCV_v100_ModernCpp_Algorithms/MSCV_v100_ModernCppFeatures/source.cpp
@@ -0,0 +1,90 @@
+#include <list>
+#include <algorithm>
+#include <iostream>
+
+
+int main()
+{
+	//---------------------------
+	//keyword auto
+	//---------------------------
+	std::list<int> ls;
+	ls.push_back(1); ls.push_back(2); ls.push_back(3);
+
+	//instead of....
+	std::list<int>::iterator it;
+	for (it = ls.begin(); it !=ls.end();++it)
+		std::cout << *it << std::endl;
+
+	//can do this...
+	for(auto it = ls.begin();it !=ls.end();++it)
+		std::cout <<*it << std::endl;
+
+	//advantage...don't need to waste brain space thinking about what the list holds,
+	//or that is even a list (in this case at least) - less imperative and more declrative
+	//(that is more "what to do", less "how to do it"). 
+
+	//const auto issues (Barney)
+	//for (const auto it = ls.begin();it!=ls.end();++it)
+	//	std::cout << *it << std::endl;
+	//But isn't this always an issue??
+
+	//---------------------------------
+
+	//-----------------------------------
+	//lamdas
+	//------------------------------------
+	const int number = 400;
+	std::for_each(ls.begin(),ls.end(),[number](int elem)
+	{elem+=number; std::cout << elem << std::endl;});
+
+	for(auto it = ls.begin();it !=ls.end();++it)
+		std::cout <<*it << std::endl;
+	//But can't do this with v100: (can in v140+) - won't compile! 
+	//std::for_each(ls.begin(),ls.end(),[number](auto elem)
+	//{elem+=number; std::cout << elem << std::endl;});
+
+	//another example
+	auto it2 = std::find_if(ls.begin(),ls.end(), [](int elem)
+			{return elem%2==1;});
+
+	std::cout << "The first odd value is" << *it2 << std::endl;
+
+	
+	
+
+	
+
+
+
+
+
+
+
+
+	//----------------------------------------
+
+
+
+	//!Bad security practice, don't do this!
+	system("pause");
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
